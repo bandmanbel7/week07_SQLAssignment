67dce5e75e157bca1f75694df22eb9497bf932ea
Robel Negatu <bandmanbel@gmail.com>
2023-11-18 20:20:09 -0500

    Week 11 Commit

--- mysql-java/src/main/java/projects/service/ProjectService.java
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 	
 	public class ProjectService {
@@ -24,6 +25,18 @@ import projects.entity.Project;
 	
 		public Project addProject(Project project) {
 			return projectDao.insertProject(project);
+		}
+
+		public void modifyProjectDetails(Project project) {
+			if(!projectDao.modifyProjectDetails(project)) {
+				throw new DbException("Project with ID=" + project.getProjectId() + " does not exist");
+			}
+		}
+		
+		public void deleteProject(Integer projectId) {
+			if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+	}
 		}
 	}
 	
